e18a6ab8a73e4e7731ff4cabf8d57c27329597a4
Aqil Hirji <amhirji@ualberta.ca>
2017-09-17 03:03:05 +0000

    second commit - updated scripts and index template
    second commit

--- static/scripts.js
+++ static/scripts.js
@@ -1,107 +1,51 @@
-$(document).ready(function() {
-    var work = true;
-    var workDuration = 25 * 60;
-    var restDuration = 5 * 60;
-    var value = 0;
-    var chip = 100 / workDuration;
-    var display = $('#time');
-    var isPaused = false;
-    var workSignal = $('#workSignal');
-    var restSignal = $('#restSignal');
-    var resetProgress = function() {
-        $('.progress-bar').css('width', '0%');
-    }
+function startTimer() {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds,
+        duration = (parseInt(($("#time").text().split(':')[0]),10) * 60) + parseInt(($("#time").text().split(':')[1]),10);
+        console.log(duration);        //parseInt((document.querySelector('#time').split(',')[0]),10) * 60 + parseInt((document.querySelector('#time').split(',')[0]),10); // remaining on the clock
+    function timer() {
+        // get the number of seconds that have elapsed since
+        // startTimer() was called
 
-    // timer function
-    function startTimer(duration, display) {
-        var timer = duration,
-            minutes, seconds;
-        var refresh = setInterval(function() {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
+        diff = duration - (((Date.now() - start) / 1000) | 0);
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
 
-            display.text(minutes + " : " + seconds);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            if (!isPaused) {
-                value += chip;
-                $('.progress-bar').css('width', value + '%');
-                if (--timer < 0) {
-                    clearInterval(refresh);
-                    var music = $("#over_music")[0];
-                    music.play();
-                    if (work === true) {
-                        display.text("Rest!");
-                        resetProgress();
-                        workSignal.css('background', '');
-                        restSignal.css('background', 'orange');
-                        value = 0;
-                        chip = 100 / restDuration;
-                        startTimer(restDuration, display);
-                        work = false;
-                    } else {
-                        display.text("Work!");
-                        resetProgress();
-                        workSignal.css('background', 'orange');
-                        restSignal.css('background', '');
-                        value = 0;
-                        chip = 100 / workDuration;
-                        startTimer(workDuration, display);
-                        work = true;
-                    }
+        document.querySelector('#time').textContent = minutes + ":" + seconds;
 
-                }
-            } else {
-                display.text("Ready");
-                resetProgress();
-                clearInterval(refresh);
-            }
-        }, 1000);
-    }
-
-    // start timer
-    startTimer(workDuration, display);
-
-    // adjust session duration
-    $('#workMore').on('click', function() {
-        isPaused = true;
-        workDuration = (parseInt($('#workTime').html(), 10) + 1) * 60;
-        $('#workTime').html(workDuration / 60);
-    });
-
-    $('#workLess').on('click', function() {
-        isPaused = true;
-        workDuration = (parseInt($('#workTime').html(), 10) - 1) * 60;
-        $('#workTime').html(workDuration / 60);
-    });
-
-    $('#restMore').on('click', function() {
-        isPaused = true;
-        restDuration = (parseInt($('#restTime').html(), 10) + 1) * 60;
-        $('#restTime').html(restDuration / 60);
-    });
-
-    $('#restLess').on('click', function() {
-        isPaused = true;
-        restDuration = (parseInt($('#restTime').html(), 10) - 1) * 60;
-        $('#restTime').html(restDuration / 60);
-    });
-
-    $('.start').click(function() {
-        if (isPaused === true) {
-            isPaused = false;
-            resetProgress();
-            value = 0;
-            chip = 100 / workDuration;
-            startTimer(workDuration, display);
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
         }
-    });
+    }
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
 
-    $('.reset').click(function() {
-        isPaused = true;
-        workSignal.css('background', 'orange');
-        restSignal.css('background', '');
-    });
-})
+function stopTimer() {
+
+}
+
+
+function getTime(){
+    var duration = (parseInt(($("#time").text().split(':')[0]),10) * 60) + parseInt(($("#time").text().split(':')[1]),10);
+    console.log(duration);
+    console.log((parseInt(($("#time").text().split(':')[0]),10) * 60) + parseInt(($("#time").text().split(':')[1]),10));
+    return duration;
+}
+
+// window.onload = function () {
+//         setTimeout(function(){
+//             getTime();
+//         }, 1000);
+// };
+
